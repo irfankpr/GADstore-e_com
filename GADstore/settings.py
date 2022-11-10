95ff4fca98bb5919d9dc017e7258f60e66431c8c
irfan <105389506+irfankpr@users.noreply.github.com>
2022-11-10 19:58:59 +0530

    Update settings.py

--- GADstore/settings.py
+++ GADstore/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'GADstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gadstore',
         'USER': 'gadstore',
         'PASSWORD': 'GAD123',
