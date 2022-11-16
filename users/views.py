723fc29de6805d726a32554d3b957dc1474ac5f3
irfan k <you@example.com>
2022-11-16 17:39:35 +0530

    dot was missing

--- users/views.py
+++ users/views.py
@@ -175,7 +175,7 @@ def home(request):
             count = 0
         new = products.objects.all().order_by('-added_date')[:12]
         cat = categories.objects.all().annotate(cat_count=Count('category_name')).order_by('category_name')
-        rndm = list(products.objects.filter())
+        rndm = list(products.objects.all())
         sug = random.sample(rndm, 8)
         for c in cat:
             c.prds = products.objects.filter(category=c.id).count()
@@ -292,7 +292,7 @@ def landing(request):
     new = products.objects.all().order_by('-added_date')[:12]
     cat = categories.objects.all().annotate(cat_count=Count('category_name')).order_by('category_name')
     subcat = sub_categories.objects.all().annotate(subcat_count=Count('sub_cat_name')).order_by('sub_cat_name')
-    rndm = list(products.objects.filter())
+    rndm = list(products.objects.all())
     sug = random.sample(rndm, 8)
     return render(request, 'gadstore.html', {'cat': cat, 'new': new, 'subcat': subcat, 'count': count, "sug":sug})
 
