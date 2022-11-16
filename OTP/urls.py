bb68763f158ca0ea7522e9d017e7ba3a460439d7
irfan k <you@example.com>
2022-11-16 11:13:08 +0530

    removed an url bcs removed an function befor

--- OTP/urls.py
+++ OTP/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from OTP import views
 
 urlpatterns = [
-                  path('otp', views.otp,name='otp'),
                   path('verify-otp', views.loginotp),
                     path('resendotp', views.reotp,name='resendotp'),
 
